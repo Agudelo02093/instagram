a9026293b3ec93bb43a48e4c52344c770306bae1
Agudelo02093 <98620510+Agudelo02093@users.noreply.github.com>
2022-12-02 19:43:24 -0500

    Delete Instagram completo directory

--- Instagram completo/public/components/Home/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Post } from "./Post/Post.js";
-export { default as Storie } from "./Stories/Stories.js";
-export { default as Suggest } from "./Suggest/Suggest.js";
-export { default as Nav } from "./Nav/Nav.js";
